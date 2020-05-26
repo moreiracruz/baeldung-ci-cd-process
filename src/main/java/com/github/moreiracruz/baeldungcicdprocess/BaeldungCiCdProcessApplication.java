0ddd75ada898eb5e6f18c8b608e23c532b11a71b
Paulo Cruz <paulo.cruz@softwareexpress.com.br>
2020-05-25 21:03:05 -0300

    Initialize application

---
+++ src/main/java/com/github/moreiracruz/baeldungcicdprocess/BaeldungCiCdProcessApplication.java
@@ -0,0 +1,13 @@
+package com.github.moreiracruz.baeldungcicdprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungCiCdProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungCiCdProcessApplication.class, args);
+	}
+
+}
